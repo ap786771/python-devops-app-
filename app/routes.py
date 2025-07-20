b96a111899eebd4995a714ab25ca9feef038e29d
ap786771 <154984891+ap786771@users.noreply.github.com>
2025-07-20 21:14:50 +0530

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, render_template, redirect, url_for
+from . import db
+from .models import Task
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@main.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    new_task = Task(title=title)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@main.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('main.index'))
